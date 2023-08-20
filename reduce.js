e8ac7b5a06ab974088de0cbf20f99144004c11ae
Mari Angeles Rojo Mira <maria.a.rojo.mira@gmail.com>
2023-08-20 23:49:59 +0000

    Examples

--- reduce.js
+++ reduce.js
@@ -7,6 +7,27 @@
 
 // Summing an array of numbers:
 
+//const nums = [0, 1, 2, 3, 4];
+//let sum = nums.reduce((acc, curr) => acc + curr);
+//console.log(sum);
+
+
+/////////////////////
+//const nums = [0, 1, 2, 3, 4];
+//let sum = nums.reduce((acc, curr) => {
+//console.log(
+  //"Accumulator", acc,
+  //"Current value", curr,
+  //"Total", acc + curr
+//);
+//return acc + curr;
+//});
+//console.log(sum);
+
+//one line code, specifying start
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
 
 const teamMembers = [
   {
